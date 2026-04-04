8b2c092f53665e48a444435f6cfc537a569cd867
manimejia <noreply@manimejia.me>
2026-04-04 14:57:09 -0500

    moved nip document to it's own repo.

--- src/tsm/announcements.ts
+++ src/tsm/announcements.ts
@@ -13,7 +13,7 @@ import { UnsignedEvent, ServiceAnnouncementConfig } from './types'
  */
 
 export const TSM_OUTPUT_KIND = '37573'
-export const TSM_GRAPERANK_NIP = 'https://github.com/nostr4social/graperank-tsm/nip.md'
+export const TSM_GRAPERANK_NIP = 'https://github.com/graperank/tsm-nip'
 
 
 export function getAllowedSubjectTypes(): string[] {
